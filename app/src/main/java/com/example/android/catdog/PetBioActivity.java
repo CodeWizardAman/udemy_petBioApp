c612225b0bc1be85fde8ce16c4c99b9a823326d6
amannn24 <amank2410@gmail.com>
2018-02-17 14:03:46 -0800

    secondary actvity update

--- app/src/main/java/com/example/android/catdog/PetBioActivity.java
+++ app/src/main/java/com/example/android/catdog/PetBioActivity.java
@@ -1,19 +1,16 @@
 package com.example.android.catdog;
 
-import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 /**
  * Created by UFO_24 on 17-02-2018.
  */
 
-public class PetBioActivity extends AppCompatActivity{
+public class PetBioActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private TextView nameTxtView;
@@ -23,13 +20,35 @@ public class PetBioActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bio);
+        imageView = (ImageView) findViewById(R.id.pet_image_view);
+        nameTxtView = (TextView) findViewById(R.id.pet_name_txt_view);
+        bioTxtView = (TextView) findViewById(R.id.pet_bio_txt_view);
 
-         imageView = (ImageView)findViewById(R.id.pet_image_view);
-         nameTxtView = (TextView)findViewById(R.id.pet_name_txt_view);
-         bioTxtView = (TextView) findViewById(R.id.pet_bio_txt_view);
+        extras = getIntent().getExtras();
 
-         extras = getIntent().getExtras();
+        if (extras != null) {
+
+            String name = extras.getString("name");
+            String bio = extras.getString("bio");
+
+            setUp(name, bio);
+        }
     }
 
-    
+    private void setUp(String name, String bio) {
+        if (name.equals("Chatori")) {
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.cat));
+
+
+        } else if (name.equals("lucky")) {
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.dog));
+
+        }
+
+        nameTxtView.setText(name);
+        bioTxtView.setText(bio);
+    }
+
+
 }
